c2c400d8d7477b00ff767778f8facc109359214b
Tommy Vinh <tommy.vinh.lam@sap.com>
2019-04-05 19:05:17 +0200

    Initial commit: Add basic template (#1)
    Initial commit: Add basic template

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("ui5.cryptocurrency.app.controller.App", {
+
+		onInit: function () {
+
+		}
+	});
+});
