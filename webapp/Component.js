bc4bde498ece2aba3e37f31a9c7b9c088f4d4c0a
Tommy Vinh <tommy.vinh.lam@sap.com>
2019-04-05 22:19:15 +0200

    Add more functionality

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function(UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/IntervalTrigger"
+], function(UIComponent, IntervalTrigger) {
 	"use strict";
 
-	return UIComponent.extend("sap.ui.demo.basicTemplate.Component", {
+	return UIComponent.extend("ui5.cryptocurrency.app.Component", {
 
 		metadata: {
 			manifest: "json"
@@ -18,19 +19,61 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
-			jQuery.ajax("model/live.json").then((oCurrencyData) => {
-				oCurrencyData.flat = [];
-				for (var sKey in oCurrencyData.rates) {
-					oCurrencyData.flat.push({
-						key: sKey,
-						rate: oCurrencyData.rates[sKey]
-					});
-				}
-				this.getModel("liveData").setData(oCurrencyData);
+			var oIntervalTrigger = new IntervalTrigger(1000),
+				aCryptoManifest, mAll;
+
+			jQuery.ajax("img/icons/manifest.json").then((aCryptoManifest) => {
+
+				mAll = aCryptoManifest.reduce((mAll, oCurrency) => {
+					mAll[oCurrency.symbol] =  oCurrency.name;
+					return mAll;
+				}, {});
+
+				jQuery.ajax("model/live.json").then((oLiveData) => {
+					this.flatten(oLiveData, mAll);
+				});
+
+				oIntervalTrigger.addListener(() => {
+					var oModel = this.getModel("liveData");
+					var oData = oModel.getData();
+					if (oData.flat) {
+						oData.flat.forEach((oNais) => {
+							oNais.rate = Math.random();
+						});
+						oModel.setData(oData);
+					}
+				});
 			});
 
 			// create the views based on the url/hash
 			this.getRouter().initialize();
+		},
+
+		flatten: function(oLiveData, mAll) {
+			var mConfig = {
+				DEFAULT: {
+					digits: 5
+				}
+			};
+
+			oLiveData.flat = [];
+			for (var sKey in oLiveData.rates) {
+				if (mAll[sKey]) {
+					mConfig[sKey] = {
+						symbol: sKey,
+					};
+
+					oLiveData.flat.push({
+						key: sKey,
+						name: mAll[sKey],
+						rate: Math.random(), //oLiveData.rates[sKey],
+						url: "img/icons/" + sKey.toLowerCase() + ".png"
+					});
+				}
+			}
+
+			this.getModel("liveData").setData(oLiveData);
+			sap.ui.getCore().getConfiguration().getFormatSettings().addCustomCurrencies(mConfig);
 		}
 	});
 });
